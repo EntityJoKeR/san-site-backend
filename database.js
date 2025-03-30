7b2e707bf06b0b7a93d4f62fa7021f24bc1ab75c
FyteBit <Fytebitsoftware@gmail.com>
2025-03-31 02:28:58 +0500

    initial

---
+++ database.js
@@ -0,0 +1,41 @@
+const sqlite3 = require('sqlite3')
+
+const db = new sqlite3.Database('./data.db', (err) => {
+    if (err) {
+      console.error("Ошибка подключения к базе:", err.message);
+    } else {
+      console.log("Подключено к базе SQLite");
+    }})
+
+const database = () => {
+    return db
+}
+
+const init = () => {
+    db.serialize(()=>{
+        db.run(`
+            create table if not exists cases (
+                id integer,
+                name text,
+                description text,
+                filename text,
+                date text
+            )
+        `)
+    })
+}
+
+const addCase = (obj) => {
+    db.run(`insert into cases values(?,?,?,?,?)`, [obj.id, obj.name, obj.description, obj.filename, obj.date])
+}
+
+//Tests
+init()
+
+addCase({
+    id: 1,
+    name: 'case name',
+    description: 'case description',
+    filename: 'file.img',
+    date: '01.01.0001'
+})
